7a3d58dd8ca490f588387dd034061a1da9d89ff4
duyating <grace_du@yunquna.com>
2021-05-13 20:47:49 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import Header from './header';
+import Sidebar from './sidebar';
+import Content from './content';
+var dom = document.getElementById('root');
+
+new Header();
+new Sidebar();
+new Content();
